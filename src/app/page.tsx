a52c2e7ec70ff04884e5cab93d405a780a079f9f
Patrik Noah Šikač <patriksikac2@gmail.com>
2024-07-24 09:02:12 +0200

    touched up landing page and sidenav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import NumberGuesserForm from "./forms/guess-form/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello world</div>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-5xl font-medium">Welcome to Exquisite Forms!</h1>
+      <h2 className="text-3xl">
+        The goal is to provide a frustrating experience to users.
+      </h2>
     </main>
   );
 }
